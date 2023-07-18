a67a4ec6709beeedb48d704c61ae0babe29e00e2
TJ Klimoski <tjklimoski@gmail.com>
2023-07-18 16:25:29 -0400

    Create floating label for text input component

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,10 +1,13 @@
+import Input from "@/components/Input";
 import Image from "next/image";
 
 export default function auth() {
   return (
     <>
       <h2 className="text-4xl mb-8 font-semibold">Sign in</h2>
-      <div className="flex flex-col gap-4"></div>
+      <div className="flex flex-col gap-4">
+        <Input />
+      </div>
     </>
   );
 }
