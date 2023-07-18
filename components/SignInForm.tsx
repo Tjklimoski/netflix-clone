0206483bcb6bbb72daa63a6bb03f378b6463fb64
TJ Klimoski <tjklimoski@gmail.com>
2023-07-18 17:46:26 -0400

    Prompt user to create an account if they don't have one

--- components/SignInForm.tsx
+++ components/SignInForm.tsx
@@ -41,6 +41,12 @@ export default function SignInForm() {
       <button className="bg-red-600 py-3 rounded-md w-full mt-10 hover:bg-red-700 transition">
         Login
       </button>
+      <p className="text-neutral-500 mt-12">
+        First time using Netflix?
+        <span className="text-white ml-1 hover:underline cursor-pointer">
+          Create an account
+        </span>
+      </p>
     </>
   );
 }
