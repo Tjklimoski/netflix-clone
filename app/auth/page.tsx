5880bf2358b05701e367b85451fe1ad97be4743f
TJ Klimoski <tjklimoski@gmail.com>
2023-07-18 18:27:15 -0400

    Add toggle functionality for sign in and sign up on auth page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -7,6 +7,7 @@ export default function auth() {
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const [isLogin, setIsLogin] = useState(true);
 
@@ -16,16 +17,20 @@ export default function auth() {
 
   return (
     <>
-      <h2 className="text-4xl mb-8 font-semibold">Sign in</h2>
+      <h2 className="text-4xl mb-8 font-semibold">
+        {isLogin ? "Sign in" : "Register"}
+      </h2>
       <div className="flex flex-col gap-4">
-        <Input
-          id="username"
-          label="Username"
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-          value={username}
-        />
+        {!isLogin && (
+          <Input
+            id="username"
+            label="Username"
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+            value={username}
+          />
+        )}
         <Input
           id="email"
           label="Email"
@@ -44,19 +49,42 @@ export default function auth() {
           type="password"
           value={password}
         />
+        {!isLogin && (
+          <Input
+            id="confirmPassword"
+            label="Confirm Password"
+            onChange={(e) => {
+              setConfirmPassword(e.target.value);
+            }}
+            type="password"
+            value={confirmPassword}
+          />
+        )}
       </div>
       <button className="bg-red-600 py-3 rounded-md w-full mt-10 hover:bg-red-700 transition">
-        Login
+        {isLogin ? "Login" : "Sign up"}
       </button>
-      <p className="text-neutral-500 mt-12">
-        First time using Netflix?
-        <span
-          onClick={toggleIsLogin}
-          className="text-white ml-1 hover:underline cursor-pointer"
-        >
-          Create an account
-        </span>
-      </p>
+      {isLogin ? (
+        <p className="text-neutral-500 mt-12">
+          First time using Netflix?
+          <span
+            onClick={toggleIsLogin}
+            className="text-white ml-1 hover:underline cursor-pointer"
+          >
+            Create an account
+          </span>
+        </p>
+      ) : (
+        <p className="text-neutral-500 mt-12">
+          Already have an account?
+          <span
+            onClick={toggleIsLogin}
+            className="text-white ml-1 hover:underline cursor-pointer"
+          >
+            Sign in
+          </span>
+        </p>
+      )}
     </>
   );
 }
