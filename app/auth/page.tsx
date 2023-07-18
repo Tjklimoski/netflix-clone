a96fe72ec38d1f30374308d98d42170e4f5b2a3c
TJ Klimoski <tjklimoski@gmail.com>
2023-07-18 18:11:08 -0400

    Set Auth page to use client, moved auth form componenet into page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,10 +1,62 @@
-import AuthForm from "@/components/AuthForm";
+"use client";
+
+import { useCallback, useState } from "react";
+import Input from "@/components/Input";
 
 export default function auth() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [isLogin, setIsLogin] = useState(true);
+
+  const toggleIsLogin = useCallback(() => {
+    setIsLogin((currentIsLogin) => !currentIsLogin);
+  }, []);
+
   return (
     <>
       <h2 className="text-4xl mb-8 font-semibold">Sign in</h2>
-      <AuthForm />
+      <div className="flex flex-col gap-4">
+        <Input
+          id="username"
+          label="Username"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+          value={username}
+        />
+        <Input
+          id="email"
+          label="Email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          type="email"
+          value={email}
+        />
+        <Input
+          id="password"
+          label="Password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          type="password"
+          value={password}
+        />
+      </div>
+      <button className="bg-red-600 py-3 rounded-md w-full mt-10 hover:bg-red-700 transition">
+        Login
+      </button>
+      <p className="text-neutral-500 mt-12">
+        First time using Netflix?
+        <span
+          onClick={toggleIsLogin}
+          className="text-white ml-1 hover:underline cursor-pointer"
+        >
+          Create an account
+        </span>
+      </p>
     </>
   );
 }
