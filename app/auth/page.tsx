959ba9131c2742052fc95592585059b993847a8e
TJ Klimoski <tjklimoski@gmail.com>
2023-07-18 18:01:19 -0400

    Change SignInForm component to AuthForm

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,10 +1,10 @@
-import SignInForm from "@/components/SignInForm";
+import AuthForm from "@/components/AuthForm";
 
 export default function auth() {
   return (
     <>
       <h2 className="text-4xl mb-8 font-semibold">Sign in</h2>
-      <SignInForm />
+      <AuthForm />
     </>
   );
 }
