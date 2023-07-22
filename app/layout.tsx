cf91559959afbbb3d3f38fd890457c8f7f111b15
TJ Klimoski <tjklimoski@gmail.com>
2023-07-22 17:26:57 -0400

    Delete SessionProvider from project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import SessionProvider from "@/components/SessionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +19,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-zinc-950 h-full overflow-x-hidden text-zinc-200`}
       >
-        <SessionProvider>{children}</SessionProvider>
+        {children}
       </body>
     </html>
   );
--- components/SessionProvider.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { SessionProvider, SessionProviderProps } from "next-auth/react";
-
-export default function Provider({ children }: SessionProviderProps) {
-  return <SessionProvider>{children}</SessionProvider>;
-}
