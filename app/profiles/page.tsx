984402501eb9904cdcff93f1aac292bedef7f4e7
TJ Klimoski <tjklimoski@gmail.com>
2023-07-22 22:54:36 -0400

    redirect user to root route when they select their profile

--- app/profiles/page.tsx
+++ app/profiles/page.tsx
@@ -1,7 +1,30 @@
+"use client";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
+
 export default function Profiles() {
+  const router = useRouter();
+  const { data: user, error, isLoading } = useCurrentUser();
+
   return (
-    <div>
-      <p className="text-4xl">Profiles</p>
+    <div className="grid place-items-center h-full">
+      <div className="flex flex-col">
+        <h1 className="text-3xl md:text-6xl text-center">Who is watching?</h1>
+        <div className="flex items-center justify-center gap-8 mt-10">
+          <div onClick={() => router.push("/")}>
+            <div className="group w-44 mx-auto hover:cursor-pointer">
+              <div className="w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:border-white overflow-hidden">
+                <img src="/images/default-blue.png" alt="" />
+              </div>
+              <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-inherit select-none">
+                {user?.name}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,5 @@
 export { default } from 'next-auth/middleware'
 
-//check for a logged in user on all routes EXCEPT the ones specified below
 export const config = {
-  matcher: '/((?!api|auth|images|_next/static|_next/image|favicon.ico).*)',
+  matcher: ['/', '/profiles'],
 }
-
-// If user is NOT logged in they will be redirected to /auth
