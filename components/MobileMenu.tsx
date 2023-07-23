5af39ded37073a1ff5cc739c78df6fc91d216601
TJ Klimoski <tjklimoski@gmail.com>
2023-07-23 15:38:15 -0400

    Create and implement toggle state for mobile menu

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -11,6 +11,13 @@ export default function MobileMenu({ visible }: MobileMenuProps) {
     <div className="bg-black w-56 absolute top-8 left-0 py-5 flex flex-col border-2 border-gray-800">
       <div className="flex flex-col gap-4">
         <div className="px-3 text-center hover:underline">Home</div>
+        <div className="px-3 text-center hover:underline">Series</div>
+        <div className="px-3 text-center hover:underline">Films</div>
+        <div className="px-3 text-center hover:underline">New & Popular</div>
+        <div className="px-3 text-center hover:underline">My List</div>
+        <div className="px-3 text-center hover:underline">
+          Browse by Languages
+        </div>
       </div>
     </div>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,23 @@
+"use client";
+
 import NavbarItem from "./NavbarItem";
 import { BiCaretDown } from "react-icons/bi";
 import MobileMenu from "./MobileMenu";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  function toggleMobileMenu() {
+    setShowMobileMenu((current) => !current);
+  }
+
   return (
     <nav className="w-full fixed z-40">
       <div className="px-4 md:px-16 py-6 flex items-center transition duration-500 bg-zinc-900 bg-opacity-90">
         <img className="h-4 lg:h-7" src="/images/logo.png" alt="logo" />
 
-        {/* The element bellow will only display on lg screens */}
+        {/* The menu bellow will only display on lg screens */}
         <div className="flex-row ml-8 gap-7 hidden lg:flex">
           <NavbarItem label="Home" />
           <NavbarItem label="Series" />
@@ -18,11 +27,14 @@ export default function Navbar() {
           <NavbarItem label="Browse by languages" />
         </div>
 
-        {/* The element bellow will only display on screens smaller then lg */}
-        <div className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
+        {/* The menu bellow will only display on screens smaller then lg */}
+        <div
+          onClick={toggleMobileMenu}
+          className="lg:hidden flex items-center gap-2 ml-8 cursor-pointer relative"
+        >
           <p className="text-sm">Browse</p>
           <BiCaretDown className="transition" />
-          <MobileMenu visible />
+          <MobileMenu visible={showMobileMenu} />
         </div>
       </div>
     </nav>
