2da7154db53c06059a9cf054a87a3b6c9ea953ca
TJ Klimoski <tjklimoski@gmail.com>
2023-07-23 12:43:40 -0400

    Setup middleware to redirect user based on logged in status and route being accessed

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,23 @@
-export { default } from 'next-auth/middleware'
+import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req });
+
+  if (token) {
+    if (req.nextUrl.pathname.startsWith('/auth')) {
+      //if logged in and navigating to login page, redirect user to root route
+      return NextResponse.redirect(new URL('/', req.nextUrl.origin))
+    }
+  } else {
+    if (!req.nextUrl.pathname.startsWith('/auth')) {
+      //if logged out and navigating to '/' or '/profiles' then redirect user to log in.
+      return NextResponse.redirect(new URL('/auth', req.nextUrl.origin))
+    }
+  }
+}
 
 export const config = {
-  matcher: ['/', '/profiles'],
+  matcher: ['/', '/profiles', '/auth'],
 }
