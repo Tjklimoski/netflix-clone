74f0d4a042643980f6fd550d8fb41419ddc340a5
TJ Klimoski <tjklimoski@gmail.com>
2023-07-24 15:28:19 -0400

    Create /api/movies/favorites GET and POST route

---
+++ app/api/movies/favorites/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse as res } from "next/server";
+import type { NextRequest } from "next/server";
+import prisma from "@/lib/prismadb";
+
+export async function GET(req: NextRequest) {
+
+}
+
+export async function POST(req: NextRequest) {
+
+}
