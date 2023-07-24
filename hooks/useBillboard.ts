82440425b21bd4fbd92b9cf652fecf13a4258b1e
TJ Klimoski <tjklimoski@gmail.com>
2023-07-23 20:58:55 -0400

    Create useBillboard hook to fetch random movie

---
+++ hooks/useBillboard.ts
@@ -0,0 +1,8 @@
+import useSWR from 'swr';=
+import fetcher from '@/lib/fetcher';
+
+export default function useBillboard() {
+  const { data, error, isLoading } = useSWR('/api/movies/random', fetcher, { revalidateIfStale: false, revalidateOnFocus: false, revalidateOnReconnect: false });
+
+  return { data, error, isLoading }
+}
