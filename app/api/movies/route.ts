8dc951d30eaac979561dfb9ec6c542c14502b3d4
TJ Klimoski <tjklimoski@gmail.com>
2023-07-23 22:53:52 -0400

    Create GET request on /api/movies route

---
+++ app/api/movies/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse as res } from "next/server";
+import type { NextRequest } from "next/server";
+import prisma from "@/lib/prismadb";
+
+export async function GET(req: NextRequest) {
+
+}
