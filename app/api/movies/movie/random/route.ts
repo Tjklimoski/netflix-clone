0d4210072d9f1e9ff97885630f54c3621cd23174
TJ Klimoski <tjklimoski@gmail.com>
2023-07-23 20:40:35 -0400

    return a single random movie from the database on /movies/movie/random route

--- app/api/movies/movie/random/route.ts
+++ app/api/movies/movie/random/route.ts
@@ -3,5 +3,18 @@ import type { NextRequest } from "next/server";
 import prisma from "@/lib/prismadb";
 
 export async function GET(req: NextRequest) {
+  try {
+    const movieCount = await prisma.movie.count();
+    const randomIndex = Math.floor(Math.random() * movieCount);
 
+    // will return an array with 1 movie document
+    const randomMovie = await prisma.movie.findMany({
+      take: 1,
+      skip: randomIndex,
+    })
+
+    return res.json(randomMovie[0])
+  } catch (err) {
+
+  }
 }
