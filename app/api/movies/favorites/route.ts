5bc1a1072018ff936af2fc479438b34481f0db0c
TJ Klimoski <tjklimoski@gmail.com>
2023-07-24 15:30:06 -0400

    Add DELETE and PUT requests to favorites route

--- app/api/movies/favorites/route.ts
+++ app/api/movies/favorites/route.ts
@@ -9,3 +9,11 @@ export async function GET(req: NextRequest) {
 export async function POST(req: NextRequest) {
 
 }
+
+export async function DELETE(req: NextRequest) {
+
+}
+
+export async function PUT(req: NextRequest) {
+
+}
