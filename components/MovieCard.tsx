94c55b4029aac67651ac89d1d9126c2c12e07881
TJ Klimoski <tjklimoski@gmail.com>
2023-07-24 18:48:33 -0400

    Shorten delay duration from 300 to 100 for MovieCard componenet animation

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -19,12 +19,12 @@ export default function MovieCard({ data }: MovieCardProps) {
   return (
     <div className="group bg-zinc-900 relative h-[12vw]">
       <img
-        className="cursor-pointer object-cover transition duration-200 shadow-xl rounded-md group-hover:opacity-90 sm:group-hover:opacity-0 delay-300 w-full  h-full"
+        className="cursor-pointer object-cover transition duration-200 shadow-xl rounded-md group-hover:opacity-90 sm:group-hover:opacity-0 delay-100 w-full  h-full"
         src={data.thumbnailUrl}
         alt={`${data.title} thumbnail`}
       />
 
-      <div className="opacity-0 absolute top-0 transition duration-200 z-10 invisible sm:visible delay-300 w-full scale-0 group-hover:scale-110 group-hover:-translate-y-[6vw] group-hover:opacity-100">
+      <div className="opacity-0 absolute top-0 transition duration-200 z-10 invisible sm:visible delay-100 w-full scale-0 group-hover:scale-110 group-hover:-translate-y-[6vw] group-hover:opacity-100">
         <img
           className="cursor-pointer object-cover transition duration-200 shadow-xl rounded-t-md w-full"
           src={data.thumbnailUrl}
