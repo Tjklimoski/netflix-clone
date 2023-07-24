5df13b013709dd773b3f8cd313a16e69d959e7ef
TJ Klimoski <tjklimoski@gmail.com>
2023-07-24 17:26:10 -0400

    Add My List MovieList componenet to home page, fetch favorites list

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,14 @@ import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
 import MovieList from "@/components/MovieList";
 import useMovieList from "@/hooks/useMovieList";
+import useFavorites from "@/hooks/useFavorites";
 
 export default function Home() {
   // we pass a defualt value of [] to data so that our value being passed
   // to MoveList data props is not undefined.
   // the type in MovieList for data does not allow for undefined.
-  const { data: movies = [], error, isLoading } = useMovieList();
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
 
   return (
     <>
@@ -18,6 +20,7 @@ export default function Home() {
       <Billboard />
       <div className="pb-40">
         <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
       </div>
     </>
   );
--- hooks/useFavorites.ts
+++ hooks/useFavorites.ts
@@ -14,7 +14,7 @@ interface Movie {
 export default function useFavorites() {
   // This is only sending a GET request to the favorites route
   // will only return an array of movie objects based on current user.
-  const { data, error, isLoading, mutate } = useSWR<Movie>('/api/movies/favorites', fetcher, { revalidateIfStale: false, revalidateOnFocus: false, revalidateOnReconnect: false });
+  const { data, error, isLoading, mutate } = useSWR<Movie[]>('/api/movies/favorites', fetcher, { revalidateIfStale: false, revalidateOnFocus: false, revalidateOnReconnect: false });
 
   return { data, error, isLoading, mutate }
 }
