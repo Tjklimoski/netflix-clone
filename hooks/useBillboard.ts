995c63df28447aba866c58c53bf7071f5ddcb45e
TJ Klimoski <tjklimoski@gmail.com>
2023-07-23 21:17:37 -0400

    Pass Movie type to useSWR in useBillboard hook

--- hooks/useBillboard.ts
+++ hooks/useBillboard.ts
@@ -1,8 +1,18 @@
 import useSWR from 'swr';
 import fetcher from '@/lib/fetcher';
 
+interface Movie {
+  id: string;
+  title: string;
+  description: string;
+  videoUrl: string;
+  thumbnailUrl: string;
+  genre: string;
+  duration: string;
+}
+
 export default function useBillboard() {
-  const { data, error, isLoading } = useSWR('/api/movies/random', fetcher, { revalidateIfStale: false, revalidateOnFocus: false, revalidateOnReconnect: false });
+  const { data, error, isLoading } = useSWR<Movie>('/api/movies/random', fetcher, { revalidateIfStale: false, revalidateOnFocus: false, revalidateOnReconnect: false });
 
   return { data, error, isLoading }
 }
