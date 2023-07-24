5497550206263f2f26abf3c7e5c5f3f65bd520cf
TJ Klimoski <tjklimoski@gmail.com>
2023-07-24 19:50:34 -0400

    Return movie from /[movieId] api route based on id passed

--- app/api/movies/[movieId]/route.ts
+++ app/api/movies/[movieId]/route.ts
@@ -9,10 +9,24 @@ interface Params {
 }
 
 export async function GET(req: NextRequest, { params }: Params) {
-  const { movieId } = params
+  const { movieId } = params;
 
   try {
+    if (!movieId || typeof movieId !== 'string') {
+      throw new Error('Invalid ID')
+    }
 
+    const movie = await prisma.movie.findUnique({
+      where: {
+        id: movieId
+      }
+    });
+
+    if (!movie) {
+      throw new Error('Invalid ID')
+    }
+
+    return res.json(movie)
   } catch (err) {
     return new NextResponse(JSON.stringify(err), { status: 400 })
   }
