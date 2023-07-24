8062868803795970dc102551c1842b171a466561
TJ Klimoski <tjklimoski@gmail.com>
2023-07-24 16:44:23 -0400

    Handle DELETE request to favorite api route

--- app/api/movies/favorite/route.ts
+++ app/api/movies/favorite/route.ts
@@ -4,16 +4,8 @@ import { without } from "lodash";
 import serverAuth from "@/lib/ServerAuth";
 import prisma from "@/lib/prismadb";
 
-export async function GET(req: NextRequest) {
-  try {
-
-  } catch (err) {
-    return new NextResponse(JSON.stringify(err), { status: 400 })
-  }
-}
-
+// Route to add a single favorite movie to the user's favoriteMovies field
 export async function POST(req: NextRequest) {
-  // Route to add a single favorite movie to the user's favoriteMovies field
   try {
     const { currentUser } = await serverAuth();
 
@@ -29,7 +21,7 @@ export async function POST(req: NextRequest) {
       throw new Error('invlaid ID')
     }
 
-    const user = await prisma.user.update({
+    const updatedUser = await prisma.user.update({
       where: {
         email: currentUser.email || ''
       },
@@ -40,25 +32,46 @@ export async function POST(req: NextRequest) {
       }
     });
 
-    return res.json(user)
+    return res.json(updatedUser)
 
   } catch (err) {
     return new NextResponse(JSON.stringify(err), { status: 400 })
   }
 }
 
+// Route to remove a single favorited movie from the user's favoriteMovies field
 export async function DELETE(req: NextRequest) {
   try {
+    const { currentUser } = await serverAuth();
 
+    const { movieId } = await req.json();
+
+    const movie = await prisma.movie.findUnique({
+      where: {
+        id: movieId
+      }
+    });
+
+    if (!movie) {
+      throw new Error('Invalid movie ID')
+    }
+
+    const updatedFavoriteIds = without(currentUser.favoriteIds, movieId)
+
+    const updatedUser = await prisma.user.update({
+      where: {
+        email: currentUser.email || ''
+      },
+      data: {
+        favoriteIds: updatedFavoriteIds
+      }
+    })
+
+    return res.json(updatedUser)
   } catch (err) {
     return new NextResponse(JSON.stringify(err), { status: 400 })
   }
 }
 
-export async function PUT(req: NextRequest) {
-  try {
-
-  } catch (err) {
-    return new NextResponse(JSON.stringify(err), { status: 400 })
-  }
-}
+// don't need GET request because when we get the user we get the favoriteIds with it.
+// don't need a PUT request because POST and DELETE handle adding and removing IDs.
