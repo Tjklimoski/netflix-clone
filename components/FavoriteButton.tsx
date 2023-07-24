ca3e207f6f326df41b5820af7ac8174d170ebd0d
TJ Klimoski <tjklimoski@gmail.com>
2023-07-24 18:44:27 -0400

    Create toggleFavorites function for adding and removing movie to users favorites

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -3,12 +3,22 @@
 import axios from "axios";
 import { useCallback, useMemo } from "react";
 import { IoAddSharp } from "react-icons/io5";
+import { AiOutlineCheck } from "react-icons/ai";
 
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useFavorites from "@/hooks/useFavorites";
 
 interface FavoriteButtonProps {
   movieId: string;
+}
+
+interface User {
+  id: string;
+  name: string;
+  image?: string;
+  email?: string;
+  emailVerified?: string;
+  favoriteIds: string[];
 }
 
 export default function FavoriteButton({ movieId }: FavoriteButtonProps) {
@@ -21,6 +31,28 @@ export default function FavoriteButton({ movieId }: FavoriteButtonProps) {
 
     return list.includes(movieId);
   }, [movieId, currentUser]);
+
+  const toggleFavorites = useCallback(async () => {
+    let response;
+
+    if (isFavorite) {
+      response = await axios.delete("/api/movies/favorites", {
+        data: { movieId },
+      });
+    } else {
+      response = await axios.post("/api/movies/favorites", { movieId });
+    }
+
+    const updatedFavoriteIds: string[] = response?.data?.favoriteIds || [];
+
+    mutate({
+      ...(currentUser as User),
+      favoriteIds: updatedFavoriteIds,
+    });
+
+    //triggers a revalidation of the data. So it will refetch the data.
+    mutateFavorites();
+  }, [movieId, isFavorite, currentUser, mutate, mutateFavorites]);
 
   return (
     <div className="cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 border-zinc-100 border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
