b718c0c360093aac21657c8a0ef4d7387999428d
TJ Klimoski <tjklimoski@gmail.com>
2023-07-23 21:21:12 -0400

    Add User type to getCurrentUser hook

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -1,8 +1,18 @@
 import useSWR from 'swr';
 import fetcher from '@/lib/fetcher';
+import { StringLiteral } from 'typescript';
+
+interface User {
+  id: string;
+  name: string;
+  image?: string;
+  email?: string;
+  emailVerified?: string;
+  favoriteIds: string[];
+}
 
 export default function useCurrentUser() {
-  const { data, error, isLoading, mutate } = useSWR('/api/auth/user', fetcher)
+  const { data, error, isLoading, mutate } = useSWR<User>('/api/auth/user', fetcher)
 
   return { data, error, isLoading, mutate }
 }
