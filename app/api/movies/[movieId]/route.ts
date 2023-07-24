4a159975c658ab2ebf1a5bb9bca1bd159c65f3ed
TJ Klimoski <tjklimoski@gmail.com>
2023-07-24 19:42:06 -0400

    Get value of dynamic route MovieId in request

--- app/api/movies/[movieId]/route.ts
+++ app/api/movies/[movieId]/route.ts
@@ -2,7 +2,15 @@ import { NextResponse, NextResponse as res } from "next/server";
 import type { NextRequest } from "next/server";
 import prisma from "@/lib/prismadb";
 
-export async function GET(req: NextRequest) {
+interface Params {
+  params: {
+    movieId: string
+  }
+}
+
+export async function GET(req: NextRequest, { params }: Params) {
+  const { movieId } = params
+
   try {
 
   } catch (err) {
