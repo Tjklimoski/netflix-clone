b20df98799d60b1c0804a6173a55e9a2d1775afc
TJ Klimoski <tjklimoski@gmail.com>
2023-07-24 17:32:07 -0400

    Check to see if movie is in current users favorites list

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -12,6 +12,16 @@ interface FavoriteButtonProps {
 }
 
 export default function FavoriteButton({ movieId }: FavoriteButtonProps) {
+  const { mutate: mutateFavorites } = useFavorites();
+  const { data: currentUser, mutate } = useCurrentUser();
+
+  // check to see if the movieId passed in as a prop is currently favorited
+  const isFavorite = useMemo(() => {
+    const list = currentUser?.favoriteIds || [];
+
+    return list.includes(movieId);
+  }, [movieId, currentUser]);
+
   return (
     <div className="cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 border-zinc-100 border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
       <IoAddSharp size={30} />
--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -24,7 +24,7 @@ export default function MovieCard({ data }: MovieCardProps) {
         alt={`${data.title} thumbnail`}
       />
 
-      <div className="opacity-0 absolute top-0 transition duration-200 z-10 invisible sm:visible delay-300 w-full scale-0 group-hover:scale-110 group-hover:-translate-y-[6vw] group-hover:translate-x-[2vw] group-hover:opacity-100">
+      <div className="opacity-0 absolute top-0 transition duration-200 z-10 invisible sm:visible delay-300 w-full scale-0 group-hover:scale-110 group-hover:-translate-y-[6vw] group-hover:opacity-100">
         <img
           className="cursor-pointer object-cover transition duration-200 shadow-xl rounded-t-md w-full"
           src={data.thumbnailUrl}
