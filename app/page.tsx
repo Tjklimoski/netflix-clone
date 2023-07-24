2ff0bb7cdf63ac1b5835bc6cf0f029e8eede6687
TJ Klimoski <tjklimoski@gmail.com>
2023-07-23 21:00:50 -0400

    Create Billboard component for home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import useCurrentUser from "@/hooks/useCurrentUser";
 import Navbar from "@/components/Navbar";
+import Billboard from "@/components/Billboard";
 
 export default function Home() {
   const { data: user, error, isLoading, mutate } = useCurrentUser();
@@ -11,6 +12,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      <Billboard />
     </>
   );
 }
---
+++ components/Billboard.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Billboard() {
+  return <div>Billboard</div>;
+}
