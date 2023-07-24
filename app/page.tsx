73c163632d6bc4e10f6f97aeaac45aa5239f22f1
TJ Klimoski <tjklimoski@gmail.com>
2023-07-23 23:53:39 -0400

    Fetch movie list and pass as data to MovieList componenet

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,20 @@ import useCurrentUser from "@/hooks/useCurrentUser";
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
 import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
 
 export default function Home() {
-  const { data: user, error, isLoading, mutate } = useCurrentUser();
+  // we pass a defualt value of [] to data so that our value being passed
+  // to MoveList data props is not undefined.
+  // the type in MovieList for data does not allow for undefined.
+  const { data: movies = [], error, isLoading } = useMovieList();
 
   return (
     <>
       <Navbar />
       <Billboard />
       <div className="pb-40">
-        <MovieList />
+        <MovieList title="Trending Now" data={movies} />
       </div>
     </>
   );
