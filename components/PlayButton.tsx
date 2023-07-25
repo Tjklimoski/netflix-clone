777722626f11020575af4dad9cdb11c4f7bfba40
TJ Klimoski <tjklimoski@gmail.com>
2023-07-24 20:29:05 -0400

    Style and add functionality to PlayButton component

--- components/PlayButton.tsx
+++ components/PlayButton.tsx
@@ -10,5 +10,13 @@ interface PlayButtonProps {
 export default function PlayButton({ movieId }: PlayButtonProps) {
   const router = useRouter();
 
-  return <div>PlayButton</div>;
+  return (
+    <button
+      onClick={() => router.push(`/watch/${movieId}`)}
+      className="bg-white text-zinc-900 rounded-md py-1 md:py-2 px-2 md:px-4 text-xs lg:text-lg font-semibold flex items-center hover:bg-neutral-300 transition"
+    >
+      <FaPlay size={16} className="mr-2" />
+      Play
+    </button>
+  );
 }
