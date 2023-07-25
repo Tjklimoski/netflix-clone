247453c0308874742aef2b02f1f8e5736b673f41
TJ Klimoski <tjklimoski@gmail.com>
2023-07-24 23:03:07 -0400

    Add info button to MovieCard with openModal functionality

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -3,6 +3,8 @@
 import { FaPlay } from "react-icons/fa";
 import FavoriteButton from "./FavoriteButton";
 import { useRouter } from "next/navigation";
+import useInfoModal from "@/hooks/useInfoModal";
+import { BiChevronDown } from "react-icons/bi";
 
 interface Movie {
   id: string;
@@ -20,6 +22,7 @@ interface MovieCardProps {
 
 export default function MovieCard({ data }: MovieCardProps) {
   const router = useRouter();
+  const { openModal } = useInfoModal();
 
   return (
     <div className="group bg-zinc-900 relative h-[12vw]">
@@ -45,6 +48,15 @@ export default function MovieCard({ data }: MovieCardProps) {
             </div>
 
             <FavoriteButton movieId={data.id} />
+            <div
+              onClick={() => openModal(data?.id)}
+              className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full grid place-items-center transition hover:border-neutral-300"
+            >
+              <BiChevronDown
+                className="group-hover/item:text-neutral-300"
+                size={30}
+              />
+            </div>
           </div>
 
           <p className="text-green-400 font-semibold mt-4">
