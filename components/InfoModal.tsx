12ff453193c3a51a326fb43db6f96c7e942e4be5
TJ Klimoski <tjklimoski@gmail.com>
2023-07-24 22:05:07 -0400

    Setup InfoModal componenet with needed state and handler functions

--- components/InfoModal.tsx
+++ components/InfoModal.tsx
@@ -1,5 +1,37 @@
-import React from "react";
+"use client";
 
-export default function InfoModal() {
+import { useCallback, useEffect, useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import PlayButton from "./PlayButton";
+import FavoriteButton from "./FavoriteButton";
+import useInfoModal from "@/hooks/useInfoModal";
+import useMovie from "@/hooks/useMovie";
+
+interface InfoModalProps {
+  visible?: boolean;
+  onClose: () => void;
+}
+
+export default function InfoModal({ visible, onClose }: InfoModalProps) {
+  const [isVisible, setIsVisible] = useState(!!visible);
+  const { movieId } = useInfoModal();
+  const { data = {} } = useMovie(movieId);
+
+  useEffect(() => {
+    setIsVisible(!!visible);
+  }, [visible]);
+
+  function handleClose() {
+    setIsVisible(false);
+    // setTimeout to allow for short close animation before triggering onClose()
+    setTimeout(() => {
+      onClose();
+    }, 300);
+  }
+
+  if (!isVisible) {
+    return null;
+  }
+
   return <div>InfoModal</div>;
 }
